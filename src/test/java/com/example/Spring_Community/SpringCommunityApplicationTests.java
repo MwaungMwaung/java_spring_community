e34c5a3a19f5c2306bdcc08a3eccf87f942d15b1
Jiseong <pupajusang01@naver.com>
2024-01-31 00:21:46 +0900

    first commit

---
+++ src/test/java/com/example/Spring_Community/SpringCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
