54503c246b9fe265cf6870aad06ecddc89f13771
Jiseong <pupajusang01@naver.com>
2024-02-01 20:15:19 +0900

    기능 추가 : 웹사이트에서 id 조회 / 전체 조회

--- src/main/java/com/example/Spring_Community/repository/ArticleRepository.java
+++ src/main/java/com/example/Spring_Community/repository/ArticleRepository.java
@@ -3,5 +3,10 @@ package com.example.Spring_Community.repository;
 import com.example.Spring_Community.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
+
+    @Override
+    ArrayList<Article> findAll();
 }
