b54f83003c30a4df148feefcd932cc0952daf27e
Jiseong <pupajusang01@naver.com>
2024-02-01 20:00:27 +0900

    기능 추가 : 웹사이트에서 id 조회 / 전체 조회

--- src/main/java/com/example/Spring_Community/controller/MemberController.java
+++ src/main/java/com/example/Spring_Community/controller/MemberController.java
@@ -6,8 +6,12 @@ import com.example.Spring_Community.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -33,4 +37,20 @@ public class MemberController {
         return "";
     }
 
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        // 레퍼지토리에서 엔티티 불러오기
+        Member memberEntitiy = memberRepository.findById(id).orElse(null);
+        // 엔티티를 모델에 등록하기
+        model.addAttribute("member", memberEntitiy);
+
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model) {
+        List<Member> memberEntityList = (List<Member>) memberRepository.findAll();
+        model.addAttribute("memberList", memberEntityList);
+        return "members/index";
+    }
 }
